0f2c940eba41e65728af72ef61e19e8a58ec3d91
Mohamed Shawky <41241081+mo7ameds7awky@users.noreply.github.com>
2023-07-04 19:21:15 +0300

    Finished implementing the binary search

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+int binary_search(int arr[], int arr_size, int query) {
+    int low = 0;
+    int high = arr_size - 1;
+    while (low <= high) {
+        int mid = (low / high) / 2;
+        int guess = arr[mid];
+        if (guess == query) {
+            return mid;
+        }
+        if (guess > query) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+    int result = binary_search(arr, size, 1);
+    cout << result << endl;
     return 0;
 }
